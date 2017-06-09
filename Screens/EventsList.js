3d8d0e9ee9f6ab1a6801ee1f56fbf0d85697436c
DaniAkash <s.daniakash@gmail.com>
2017-06-09 20:52:52 +0530

    update eventslist

--- Screens/EventsList.js
+++ Screens/EventsList.js
@@ -1,74 +1,70 @@
-import React, { Component, PureComponent } from 'react';
+import React, { Component } from 'react';
 import {
-  Text,
-  View,
   FlatList,
+  View,
+  Text,
   TouchableHighlight,
 } from 'react-native';
 
-import events from '../data/events';
+import eventList from '../data/eventList.js';
 
 export default class EventsList extends Component {
+  static navigationOptions = {
+    title: 'Event List'
+  }
 
-    static navigationOptions = {
-        title: 'Events List',
-    };
+  state = {
+    refreshing: false,
+  }
 
-    state = {
-        refreshing: false,
-        events
-    };
+  _refreshList() {
+    this.setState({refreshing: true});
+    setTimeout(() => {
+      this.setState({refreshing: false});
+    }, 3000)
+  }
 
-    _refresh() {
-      this.setState({refreshing: true});
-      setTimeout(() => {
-        this.setState({refreshing: false});
-        return;
-      }, 3000);
-    }
+  _keyExtractor(item, index) {
+    return item.id;
+  }
 
-    _keyExtractor(item, index) {
-        return item.id;
-    }
+  _renderRow(item, index) {
+    return(
+      <TouchableHighlight
+        onPress={() => this.props.navigation.navigate('Registration', {name: item.event})}
+        underlayColor="silver"
+        >
+        <View style={{
+          paddingHorizontal: 10,
+          paddingVertical: 5,
+          backgroundColor: index%2===0? 'white': '#F0FFF0'
+          }}>
+          <Text style={{marginBottom: 10}}>{item.event}</Text>
+          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+            <Text>{item.location}</Text>
+            <Text>{item.date}</Text>
+          </View>
+        </View>
+      </TouchableHighlight>
+    )
+  }
 
-    _renderRow(item) {
-        return (
-            <TouchableHighlight
-                onPress={()=>{
-                    this.props.navigation.navigate('Registration', { name: item.event });
-                }}
-                underlayColor='transparent'>
-                <View style={{flexDirection: 'row', justifyContent: 'space-between', marginVertical: 10}}>
-                    <View style={{height: 50, justifyContent: 'space-between'}}>
-                        <Text>{item.event}</Text>
-                        <Text>{item.location}</Text>
-                    </View>
-                    <View>
-                        <Text>{item.date}</Text>
-                    </View>
-                </View>
-            </TouchableHighlight>
-        );
-    }
+  _renderSeperator() {
+    return(
+      <View style={{backgroundColor: 'black', height: 0.5}}></View>
+    );
+  }
 
-    _renderSeperator() {
-        return(
-            <View style={{backgroundColor: 'black', height: 1}}></View>
-        )
-    }
-
-    render() {
-        return(
-            <View>
-                <FlatList
-                    refreshing={this.state.refreshing}
-                    onRefresh={this._refresh.bind(this)}
-                    data={events}
-                    renderItem={({item}) => this._renderRow(item)}
-                    keyExtractor={this._keyExtractor}
-                    ItemSeparatorComponent={this._renderSeperator}
-                />
-            </View>
-        );
-    }
+  render() {
+    return(
+      <FlatList
+        refreshing={this.state.refreshing}
+        onRefresh={this._refreshList.bind(this)}
+        data={eventList}
+        keyExtractor={this._keyExtractor}
+        renderItem={({item, index})=>this._renderRow(item, index)}
+        ItemSeparatorComponent={this._renderSeperator}
+      />
+    )
+  }
 }
